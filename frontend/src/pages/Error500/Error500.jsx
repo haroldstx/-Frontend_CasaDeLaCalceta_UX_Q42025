daae5807a31a2d10cf6bde816a3bb5ae66ba782b
Nickyi175 <tome.andrea1378@gmail.com>
2025-12-10 23:51:13 -0600

    Page de Error 500

---
+++ frontend/src/pages/Error500/Error500.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Error500Component from "../../components/error500/error500";
+
+const Error500 = () => {
+  return <Error500Component />;
+};
+
+export default Error500;
