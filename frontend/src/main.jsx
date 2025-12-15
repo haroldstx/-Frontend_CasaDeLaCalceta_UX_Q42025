bd92fc74149876fc186a5d23f76a07821d7f4b0d
Nickyi175 <tome.andrea1378@gmail.com>
2025-12-15 01:46:59 -0600

    Creacion de page de Editar_perfil

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import NavbarAdmin from "./components/NavbarAdmin/NavbarAdmin.jsx";
 import NavbarCliente from "./components/NavbarCliente/NavbarCliente.jsx";
 import Error500 from "./pages/Error500.jsx";
 import VerMiPerfilPage from "./pages/VerMiPerfilPage.jsx";
+import EditarPerfil from "./pages/EditarPerfilPage.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -21,7 +23,11 @@ createRoot(document.getElementById("root")).render(
         <Route path="*" element={<ErrorPage404 />} />
         <Route path="/error-500" element={<Error500 />} />
         <Route path="/ver-mi-perfil" element={<VerMiPerfilPage />} />
+        <Route path="/editar-perfil" element={<EditarPerfil />} />
       </Routes>
     </BrowserRouter>
+    <ToastContainer
+
+    />
   </StrictMode>
 );
